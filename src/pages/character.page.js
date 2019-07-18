0a38e6ae6d6c90aa08425cd67ad21fbab10fd7e1
Balbaal <hai.balbaal@gmail.com>
2019-07-18 16:27:42 +0700

    Fix: solved character page & add loading

--- src/pages/character.page.js
+++ src/pages/character.page.js
@@ -1,15 +1,78 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout.component"
+import Layout from "../components/layout.component";
+import Title from "../components/title.component";
 
 class Character extends React.Component {
-	render() {
-		return (
-			<Layout>
-				Character page
-			</Layout>
-		)
+  constructor(props) {
+    super(props);
+
+    this.state = {
+			movie: {},
+			characters: [],
+			isLoading: false
+    };
 	}
+	
+	generateCharacters = (charactersInit) => {
+		this.setState({isLoading: true})
+		let resCharacters = charactersInit.map(async character => {
+			const resCharacter = await fetch(character)
+			const finalResCharacter = resCharacter.json()
+			return finalResCharacter
+		})
+
+		return Promise.all(resCharacters)
+	}
+
+  componentDidMount() {
+    const resMovie = this.props.movies.find(
+      res => res.episode_id === parseInt(this.props.urlParams.episode_id)
+    );
+		this.setState({ movie: resMovie });
+		
+		// generate that characters
+		this.generateCharacters(resMovie.characters)
+			.then(resData => {
+				this.setState({characters: resData})
+				this.setState({isLoading: false})
+			})
+  }
+
+  render() {
+		console.log(this.state.characters)
+    const styles = {
+      container: {
+        padding: "0.5em 1.5em",
+        backgroundColor: "black",
+        borderRadius: "3px"
+      },
+      list: {
+        marginBottom: "0.5em"
+      }
+    };
+
+    return (
+      <Layout>
+        <Title
+          director={`from movie: ${this.state.movie.title}`}
+          title={`All Characters`}
+        />
+        <div style={styles.container}>
+          {
+						this.state.isLoading ? <h2 style={{color: "yellow"}}>loading . . .</h2> :
+						<ul>
+            {
+							this.state.characters.map((character, i) => (
+								<li key={i} style={styles.list}>{character.name}</li>
+							))
+						}
+          </ul>
+					}
+        </div>
+      </Layout>
+    );
+  }
 }
 
-export default Character
+export default Character;
